43163c30e4f5893735669a431ad75d9d2ac102bb
Niranjan-Kumar-Gupta <guptaniranjan8896069078@gmail.com>
2022-08-06 14:30:38 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
+import List from './componenets/List';
+
+
+interface IState {
+  crypto:{
+    coinType:string
+    img:string
+    price:number 
+    tvl:number 
+    percent:number
+  }[]
+}
 
 function App() {
+  const [crypto,setCrypto] = useState<IState["crypto"]>([{
+    coinType:'Bitcoin(BTC)',
+    img:'11.png',
+    price:24512,
+    tvl:78500,
+    percent:10
+  },
+  {
+    coinType:'Ethereium(ETH)',
+    img:'2.png',
+    price:94512,
+    tvl:60000,
+    percent:-12.32
+  },
+  {
+    coinType:'Riple(RIP)',
+    img:'3.png',
+    price:7412,
+    tvl:50140,
+    percent:-11.23
+  },
+  {
+    coinType:'Neo(NEO)',
+    img:'4.png',
+    price:2512,
+    tvl:40000,
+    percent:10
+  },
+  {
+    coinType:'Monero(MON)',
+    img:'5.png',
+    price:4412,
+    tvl:47580,
+    percent:30
+  }
+
+
+])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <List crypto={crypto} />
     </div>
   );
 }
